929628cf3cb3577d7afd9c784a6e36b5fbe97cdb
liuchaos03 <49407026+liuchaos03@users.noreply.github.com>
2022-02-10 16:48:10 +0800

    Delete Considering customers non rationalities directory

--- Considering customers non rationalities/getlinkNN.m
+++
@@ -1,7 +0,0 @@
-function  Result=getlinkNN(Alpha,Beta,G,tmp,rank)
-
-      parfor i=1:rank
-%          Result(i,:)=linkIC4(Alpha,Beta,G,tmp(i,:));
-           Result(i,:)=linkNN2(Alpha,Beta,G,tmp(i,:));
-      end
-end
